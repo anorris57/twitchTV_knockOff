8b50ee58d9cbf808fd198ddc7c9ced3530e883fe
Angela Norris <cocoanorris@yahoo.com>
2019-09-17 10:12:32 -0400

    adding BrowseRouter and Route from react-routerdom

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
+//Browser Router and Route are actual components
+import { BrowserRouter, Route } from 'react-router-dom';
+//react router cares only about characters after domain localhost:3000 = localhost:3000/
+
+const PageOne = () => {
+  return <div>PageOne</div>;
+};
+
+const PageTwo = () => {
+  return <div>
+    PageTwo
+    <button> Click Me! </button>
+    </div>;
+}
 
 const App = () => {
   return (
-    <div>
-      App
-    </div>
+      <BrowserRouter>
+        <div>
+        <Route path="/" exact component={PageOne}/>
+        <Route path="/pagetwo" component={PageTwo}/>
+        </div>
+      </BrowserRouter>
   )
 }
 
