5f1b1801cda7c0c7d21be0ddd66e2f6519ff2137
Angela Norris <cocoanorris@yahoo.com>
2019-09-18 09:57:03 -0400

    adding Links inside of routers

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
 //Browser Router and Route  and Link (use instead of a tag)are actual components
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route} from 'react-router-dom';
 //react router cares only about characters after domain localhost:3000 extracted 'path'starts with /
 import StreamCreate from './streams/StreamCreate';
 import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
+import Header from './Header';
 
 
 
 const App = () => {
   return (
+    <div className="ui container">
       <BrowserRouter>
         <div>
+          <Header />
           <Route path="/"  exact component={StreamList} />
           <Route path="/streams/new"  exact component={StreamCreate} />
           <Route path="/streams/edit"  exact component={StreamEdit} />
@@ -21,6 +24,7 @@ const App = () => {
           <Route path="/streams/show"  exact component={StreamShow} />
         </div>
       </BrowserRouter>
+    </div>
   )
 }
 
