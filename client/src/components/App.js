a6aa8347d2dc5809d3443c25dce31c0adb6b7103
Angela Norris <cocoanorris@yahoo.com>
2019-09-18 08:50:53 -0400

    adding Link tags instead of a tags to navigate

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
-//Browser Router and Route are actual components
-import { BrowserRouter, Route } from 'react-router-dom';
+//Browser Router and Route  and Link (use instead of a tag)are actual components
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 //react router cares only about characters after domain localhost:3000 extracted 'path'starts with /
 
 const PageOne = () => {
-  return <div>PageOne</div>;
+  return (
+  <div>
+    PageOne
+    <Link to="/pagetwo">Navigate to Page Two</Link>
+    </div>
+  )
 };
 
 const PageTwo = () => {
-  return <div>
-    PageTwo
-    <button> Click Me! </button>
-    </div>;
+  return (
+    <div>
+      PageTwo
+      <button> Click Me! </button>
+      <Link to="/">Navigate to Page One</Link> 
+    </div>
+  ) 
 }
 
 const App = () => {
