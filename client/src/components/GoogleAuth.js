66b476ac9604158ba95dad77ce92339200dcce8d
Angela Norris <cocoanorris@yahoo.com>
2019-09-24 16:33:30 -0400

    grabbing goggle id of user

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -19,7 +19,7 @@ class GoogleAuth extends React.Component {
 
   onAuthchange = (isSignedIn) => {
     if (isSignedIn) {
-      this.props.signIn();
+      this.props.signIn(this.auth.currentUser.get().getId());
     } else {
       this.props.signOut();
     }
