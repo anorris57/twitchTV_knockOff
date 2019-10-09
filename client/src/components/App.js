d3ac688d948d2c244e02a1c3bc5185071def2aae
Angela Norris <cocoanorris@yahoo.com>
2019-10-09 13:50:16 -0400

    programmatic navigation using history object

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 //Browser Router and Route  and Link (use instead of a tag)are actual components
-import { BrowserRouter, Route} from 'react-router-dom';
+import { Router, Route} from 'react-router-dom';
 //react router cares only about characters after domain localhost:3000 extracted 'path'starts with /
 import StreamCreate from './streams/StreamCreate';
 import StreamEdit from './streams/StreamEdit';
@@ -8,13 +8,14 @@ import StreamDelete from './streams/StreamDelete';
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
 import Header from './Header';
+import history from '../history';
 
 
 
 const App = () => {
   return (
     <div className="ui container">
-      <BrowserRouter>
+      <Router history={history}>
         <div>
           <Header />
           <Route path="/"  exact component={StreamList} />
@@ -23,7 +24,7 @@ const App = () => {
           <Route path="/streams/delete"  exact component={StreamDelete} />
           <Route path="/streams/show"  exact component={StreamShow} />
         </div>
-      </BrowserRouter>
+      </Router>
     </div>
   )
 }
